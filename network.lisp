cac2a71a36b7f80e68511af7231ffc8b8950ac45
Marcio Faustino <marciof@home>
2011-02-19 17:17:22 +0100

    Fixed byte stream reading with ECL 11.1.1 and Windows 7 64-bit.

--- network.lisp
+++ network.lisp
@@ -65,7 +65,15 @@
 
 (definterface read-octets (buffer connection)
   (:implementation t
-    (read-sequence buffer connection))
+    ;; ECL bug #3161786.
+    (if (equal (stream-element-type connection)
+               '(unsigned-byte 8))
+      (read-sequence buffer connection)
+      (let ((length (array-dimension buffer 0)))
+        (loop for i from 0 to (1- length) do
+          (setf (aref buffer i)
+                (read-byte connection)))
+        length)))
   (:implementation allegro
     (ql-allegro:read-vector buffer connection))
   (:implementation clisp
