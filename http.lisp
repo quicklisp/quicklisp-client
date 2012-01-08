9918e6693f0ef7c46a2792d8e616e99777d97d1f
Zach Beane <xach@xach.com>
2012-01-07 20:56:36 -0500

    Update version to 2012010700.

--- http.lisp
+++ http.lisp
@@ -350,7 +350,7 @@ information."
                    (encode (subseq string start limit))))))
     ;; FIXME: Be more configurable, and take/set the version from
     ;; somewhere else.
-    (format nil "quicklisp-client/2012010600 ~A/~A"
+    (format nil "quicklisp-client/2012010700 ~A/~A"
             (encode (lisp-implementation-type))
             (version-string (lisp-implementation-version)))))
 
--- quicklisp.asd
+++ quicklisp.asd
@@ -5,7 +5,7 @@
   :author "Zach Beane <zach@quicklisp.org>"
   :license "BSD-style"
   :serial t
-  :version "2012010600"
+  :version "2012010700"
   :components ((:file "package")
                (:file "utils")
                (:file "config")
