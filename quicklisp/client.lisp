f39e40c2da060a9d270e7a85b5a85e1bc9b70058
Jéssica Milaré <jessymilare@gmail.com>
2019-06-12 12:13:05 -0300

    Add :PRINT-CASE argument to quickload, set :UPCASE by default
    Some systems don't load correctly when *PRINT-CASE* isn't :UPCASE.

--- quicklisp/client.lisp
+++ quicklisp/client.lisp
@@ -20,16 +20,18 @@
 (defun maybe-silence (silent stream)
   (or (and silent (make-broadcast-stream)) stream))
 
-(defgeneric quickload (systems &key verbose silent prompt explain &allow-other-keys)
+(defgeneric quickload (systems &key verbose silent prompt explain print-case &allow-other-keys)
   (:documentation
    "Load SYSTEMS the quicklisp way. SYSTEMS is a designator for a list
    of things to be loaded.")
   (:method (systems &key
             (prompt *quickload-prompt*)
             (silent nil)
-            (verbose *quickload-verbose*) &allow-other-keys)
+            (verbose *quickload-verbose*)
+            (print-case :upcase) &allow-other-keys)
     (let ((*standard-output* (maybe-silence silent *standard-output*))
-          (*trace-output*    (maybe-silence silent *trace-output*)))
+          (*trace-output*    (maybe-silence silent *trace-output*))
+          (*print-case*      print-case))
       (unless (consp systems)
         (setf systems (list systems)))
       (dolist (thing systems systems)
